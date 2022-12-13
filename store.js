aca0be82d25ad0dbda5978b23b3c8bd298a59ec5
Rajlaxmi Das <rajlaxmidas.77@gmail.com>
2022-12-13 21:54:55 +0530

    delete and edit

--- store.js
+++ store.js
@@ -14,28 +14,50 @@ function save(e){
     };
     
     item.push(i);
-    
-localStorage.setItem(`user ${item}`,JSON.stringify(myObj));
-display(myObj);
+    display(myObj);   
+localStorage.setItem(myObj.email,JSON.stringify(myObj));
+
  
 }
-
-function display(myObj){
-   // const ul= document.getElementById("users");
-    const li = document.createElement("li");
+const li = document.createElement("li");
     li.className = "items";
+    
+function display(myObj){
+    if(localStorage.getItem(myObj.email)!==null){
+        li.remove();
+    }
+   
     li.appendChild(
         document.createTextNode("Name : "+myObj.nameee + ", EmailId : " + myObj.email + " ")
       );
-      var editBtn = document.createElement('button');
-      var delBtn = document.createElement('button');
-      
-      editBtn.appendChild(document.createTextNode('EDIT'));
-      delBtn.appendChild(document.createTextNode('DELETE'));
-      delBtn.style.backgroundColor = "red";
-      editBtn.style.backgroundColor="green";
-      li.appendChild(editBtn);
-      li.appendChild(delBtn);
+
+  const a1 = document.createElement("input");
+  
+  a1.type = "button";
+  a1.value = "Edit";
+  a1.addEventListener("click", () => {
+    
+    document.getElementById("name").value = myObj.nameee;
+    document.getElementById("email").value = myObj.email;
+    li.remove();
+  });
+  a1.className = "delete";
+  a1.style.border = "2px solid green";
+  
+  li.appendChild(a1);
+  
+  const a = document.createElement("input");
+  a.type = "button";
+  a.value = "delete";
+  a.addEventListener("click", () => {
+    localStorage.removeItem(myObj.email);
+    li.remove();
+  });
+  a.className = "delete";
+  a.style.border = "2px solid red";
+  
+  li.appendChild(a);
+  
      itemList.append(li);
 }
 
